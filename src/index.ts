8841814b426b268d8df53ffa1751288eca6d5874
Paula Santamaría <m.paula.santamaria@gmail.com>
2023-07-03 20:32:28 -0300

    use classes and fix entry navigation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { getFeed } from "./rss.service.js";
-import { printEntryList } from "./ui/entry-list.ui.js";
-
+import { EntryListUI } from "./ui/entry-list.ui.js";
 
 //const rssUrl = 'https://blog.paulasantamaria.com/rss.xml'; // Hashnode
 const rssUrl = 'https://dev.to/feed/paulasantamaria'; // Dev.to
 
 const feed = await getFeed(rssUrl);
 
-console.log(printEntryList(feed.entries));
+new EntryListUI(feed.entries).load();
--- src/ui/entry-list.ui.ts
+++ src/ui/entry-list.ui.ts
@@ -1,28 +1,115 @@
+import { Controller } from '../controller.js';
 import type { Entry } from '../rss.service.js';
 import { getBorderCharacters, table } from 'table';
 
-let selectedIndex = 0;
+export class EntryListUI {
+    private entries: Entry[];
+    private selectedIndex = 0;
+    private currentPage = 0;
 
-export function printEntryList(entries: Entry[]) {
-    const tableData = [];
+    private totalPages = 0;
+    private entriesPerPage = 0;
+    private screenSize = { columns: 0, rows: 0 };
 
-    for (let i = 0; i < entries.length; i++) 
-        tableData.push([selectedIndex == i ? '>': ' ', entryToString(entries[i])]);
+    private readonly entryRowHeight = 7;
+    private readonly entryTableHeaderHeight = 3;
+    private readonly entryTableFooterHeight = 3;
+    private readonly controller = new Controller();
 
-    return table(tableData, {
-        border: getBorderCharacters('norc'),
-        header: { content: 'FEED ENTRIES' },
-        drawHorizontalLine: () => true,
-        drawVerticalLine: () => false,
-    });
-}
+    constructor(entries: Entry[]) {
+        this.entries = entries;
+        this.screenSize = { columns: process.stdout.columns, rows: process.stdout.rows };
+    }
 
-function entryToString(entry: Entry): string {
-    let entryRow = `${entry.title?.toUpperCase() ?? 'TITLE NOT FOUND'}\n`;
-    if (entry.date) entryRow += `${entry.date.toLocaleDateString()} | `;
-    if (entry.creator) entryRow += `${entry.creator}\n`;
-    if (entry.categories) entryRow += `${entry.categories}\n`;
-    if (entry.url) entryRow += `${entry.url.slice(0, 48)}${entry.url.length > 48 ? '...' : ''}\n`;
+    load() {
+        this.entriesPerPage = Math.floor(
+            (this.screenSize.rows - (this.entryTableHeaderHeight + this.entryTableFooterHeight)) /
+                this.entryRowHeight
+        );
+        this.totalPages = Math.ceil(this.entries.length / this.entriesPerPage);
 
-    return entryRow;
+        this.controller
+            .on('up', () => {
+                this.move('up');
+            })
+            .on('down', () => {
+                this.move('down');
+            })
+            .on('return', () => {
+                console.log('TODO navigate to entry UI');
+                this.controller.clear(); // Clear commands so the entry UI can add its own.
+            })
+            .build();
+
+        console.clear();
+        this.print();
+    }
+
+    private print() {
+        console.clear();
+        process.stdout.cursorTo(0, 0);
+
+        const tableData = [];
+        const startIndex = this.currentPage * this.entriesPerPage;
+        const currentPageEntries = this.entries.slice(startIndex, startIndex + this.entriesPerPage);
+
+        for (let i = 0; i < currentPageEntries.length; i++)
+            tableData.push([
+                this.selectedIndex == i ? '\n>' : ' ',
+                this.entryToString(currentPageEntries[i])
+            ]);
+
+        console.clear();
+        console.log(
+            table(tableData, {
+                border: getBorderCharacters('norc'),
+                header: { content: 'MY FEED' },
+                drawHorizontalLine: () => true,
+                drawVerticalLine: () => false
+            })
+        );
+        this.printFooter();
+        console.log('\u001B[?25l'); // Hide cursor.
+    }
+
+    private entryToString(entry: Entry): string {
+        let entryRow =
+            this.sliceTextToFit(entry.title?.toUpperCase() ?? 'TITLE NOT FOUND') + '\n\n';
+        entryRow += this.sliceTextToFit(entry.date?.toLocaleDateString() ?? '') + ' | ';
+        entryRow += this.sliceTextToFit(entry.creator ?? '') + '\n';
+        entryRow += this.sliceTextToFit(entry.categories?.join(', ') ?? '');
+
+        return `\n${entryRow}\n`;
+    }
+
+    private printFooter() {
+        let footer = `Page ${this.currentPage + 1} of ${this.totalPages}`;
+        footer += ` | ${this.entries.length} entries`;
+
+        console.log(footer);
+    }
+
+    private sliceTextToFit(text: string) {
+        if (text.length < this.screenSize.columns - 4) return text;
+
+        return text.slice(0, this.screenSize.columns - 8) + '...';
+    }
+
+    private move(direction: 'up' | 'down') {
+        if (direction == 'up') {
+            this.selectedIndex--;
+            if (this.selectedIndex < 0 && this.currentPage > 0) {
+                this.currentPage--;
+                this.selectedIndex = this.entriesPerPage - 1;
+            }
+        } else {
+            this.selectedIndex++;
+            if (this.selectedIndex >= this.entriesPerPage && this.currentPage < this.totalPages) {
+                this.selectedIndex = 0;
+                this.currentPage++;
+            }
+        }
+
+        this.print();
+    }
 }
